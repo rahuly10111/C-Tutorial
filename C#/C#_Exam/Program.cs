3df0fbe77e8f11ad56018ba0b552f1f9c6e8d410
Rahul Yadav <rahuly10111@gmail.com>
2023-04-11 18:41:30 +0530

    tasks update

--- C#/C#_Exam/Program.cs
+++ C#/C#_Exam/Program.cs
@@ -6,6 +6,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -34,6 +35,7 @@ namespace C__Exam
             var isout = false;
             do
             {
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine(" Kindly,\n Type 1 to Add New Detail : , \n Type 2 to Delete Data :");
                 Console.Write("Type the Number as per Above Message : ");
                 int collegefeature = Convert.ToInt16(Console.ReadLine());
@@ -42,22 +44,18 @@ namespace C__Exam
 
                 {
                     case 1:
-                        AddEmployeeDetail(fileDetail, employeeDetailList);
+                        AddEmployeeDetail(employeeDetailList);
                         break;
                     case 2:
                         FileDataRead();
                         RemoveEmployee(employeeDetailList);
-                        //  removeEmployee(employeeDetailList);
                         break;
-
-
-
                     default:
                         Console.WriteLine(" Only Type the Number as per Above Message ");
                         break;
                 }
                 Console.WriteLine("\n TO Continue Watching Tasks -- Press (3) \n");
-                var input = Console.ReadLine();
+                var input = Console.ReadLine().Trim();
                 if (input == "3")
                 {
                     isout = true;
@@ -88,7 +86,7 @@ namespace C__Exam
             public double employeeTotalExperience;
             public string employeeRemark;
             public string employeeDepartMent;
-            public int employeeMonthlySalary;
+            public double employeeMonthlySalary;
         }
 
         public enum EmployeeDepartMentenum
@@ -108,8 +106,7 @@ namespace C__Exam
 
         }
 
-
-        public static void AddEmployeeDetail(string fileDetail, List<EmployeeData> employeeDetailList)
+        public static void AddEmployeeDetail(List<EmployeeData> employeeDetailList)
         {
             EmployeeData employeeRecordDetail = new EmployeeData();
 
@@ -120,6 +117,7 @@ namespace C__Exam
             {
                 try
                 {
+
                     Console.Write(" Enter Employee Id :  ");
                     employeeRecordDetail.employeeId = Convert.ToInt16(Console.ReadLine().Trim());
                     if (UserId(employeeRecordDetail.employeeId) == true)
@@ -138,11 +136,25 @@ namespace C__Exam
 
             }
 
-            do
+            bool isValidName = false;
+            while (!isValidName)
             {
                 Console.Write(" Enter Employee Name : ");
                 employeeRecordDetail.employeeName = Convert.ToString(Console.ReadLine());
-            } while (!NameValidation(employeeRecordDetail.employeeName));
+
+                if (!NameValidation(employeeRecordDetail.employeeName))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" -> Invalid name. Please enter a valid name.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    isValidName = true;
+                }
+            }
+
+
 
             bool birthDate = true;
             while (birthDate)
@@ -154,74 +166,147 @@ namespace C__Exam
                 {
                     DateTime getBirthDate = Convert.ToDateTime(dateofbirth);
                     employeeRecordDetail.employeeDOB = DateExtensions.DateFormate(getBirthDate);
-                    //  Console.WriteLine("Your Date of Joining - " + employeeRecordDetail.employeeDOB);
-
 
                     birthDate = false;
                 }
                 catch (FormatException)
                 {
-                    Console.WriteLine("Invalid Date Format.");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" -> Invalid Date Format.");
+                    Console.ForegroundColor = ConsoleColor.White;
                     continue;
                 }
             }
 
-
-            Console.WriteLine(" Please Type M for Male and F for Female ");
-            Console.Write(" Enter Employee Gender : ");
-            string gender = Convert.ToString(Console.ReadLine().ToLower().Trim());
-            switch (gender)
+             bool isgendervalid=false;
+            while (!isgendervalid)
             {
-                case "m":
-                    employeeRecordDetail.employeeGender = "Male";
-                    break;
-                case "f":
-                    employeeRecordDetail.employeeGender = " Female ";
-                    break;
-                default:
-                    Console.WriteLine(" Kindly Type M for Male and F for Female ");
-                    break;
+                Console.WriteLine(" Please Type M for Male and F for Female ");
+                Console.Write(" Enter Employee Gender : ");
+                string gender = Convert.ToString(Console.ReadLine().ToLower().Trim());
+                switch (gender)
+                {
+                    case "m":
+                        employeeRecordDetail.employeeGender = "Male";
+                        isgendervalid = true;
+                        break;
+                    case "f":
+                        employeeRecordDetail.employeeGender = " Female ";
+                        isgendervalid = true;
+                        break;
+                    default:
+                        Console.WriteLine(" -> Ivalid Input  ");
+                        break;
+                }
             }
+            
 
 
-            do
+            bool isValidDesignation = false;
+            while (!isValidDesignation)
             {
                 Console.Write(" Enter Employee Designation : ");
                 employeeRecordDetail.employeeDesignation = Convert.ToString(Console.ReadLine());
-            } while (!DesignationValidation(employeeRecordDetail.employeeDesignation));
 
-            do
+                if (!DesignationValidation(employeeRecordDetail.employeeDesignation))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" -> Invalid Designation. Please enter a valid Designation.");
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                }
+                else
+                {
+                    isValidDesignation = true;
+                }
+            }
+
+            bool isStateValid = false;
+            while (!isStateValid)
             {
                 Console.Write(" Enter Employee State : ");
                 employeeRecordDetail.employeeState = Convert.ToString(Console.ReadLine());
-            } while (!StringValidation(employeeRecordDetail.employeeState));
+                if (!StringValidation(employeeRecordDetail.employeeState))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" -> Invalid State Name. Please enter a valid State Name.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    isStateValid = true;
+                }
+            }
 
-            do
+
+            bool isCityValid = false;
+            while (!isCityValid)
             {
                 Console.Write(" Enter Employee City : ");
                 employeeRecordDetail.employeeCity = Convert.ToString(Console.ReadLine());
-            } while (!StringValidation(employeeRecordDetail.employeeCity));
+                if (!StringValidation(employeeRecordDetail.employeeCity))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" -> Invalid City Name. Please enter a valid City Name .");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    isCityValid = true;
+                }
+            }
 
-            do
+            bool ispostcodeValid = false;
+            while (!ispostcodeValid)
             {
                 Console.Write(" Enter Employee PostCode : ");
                 employeeRecordDetail.employeePostCode = Convert.ToString(Console.ReadLine());
-            } while (!PostCodeValidation(employeeRecordDetail.employeePostCode));
+                if (!PostCodeValidation(employeeRecordDetail.employeePostCode))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" -> Invalid PostCode. Please enter a valid PostCode.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    ispostcodeValid = true;
+                }
+            }
 
 
-            do
+            bool isPhonenoValid = false;
+            while (!isPhonenoValid)
             {
                 Console.Write(" Enter Employee Phone Number : ");
                 employeeRecordDetail.employeePhoneNo = Convert.ToString(Console.ReadLine());
-            } while (!PhonenoValidation(employeeRecordDetail.employeePhoneNo));
+                if (!PhonenoValidation(employeeRecordDetail.employeePhoneNo))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" -> Invalid Phone Number. Please enter a valid Phone Number.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    isPhonenoValid = true;
+                }
+            }
 
-
-            do
+            bool isEmailIdValid = false;
+            while (!isEmailIdValid)
             {
                 Console.Write(" Enter Employee Email ID : ");
                 employeeRecordDetail.employeeEmail = Convert.ToString(Console.ReadLine());
-            } while (!EmailValidation(employeeRecordDetail.employeeEmail));
-
+                if (!EmailValidation(employeeRecordDetail.employeeEmail))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" -> Invalid Email ID. Please enter a valid Email ID.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    isEmailIdValid = true;
+                }
+            }
 
 
             bool joinDate = true;
@@ -233,73 +318,101 @@ namespace C__Exam
                 {
                     DateTime getJoinDate = Convert.ToDateTime(dateOfJoin);
                     employeeRecordDetail.employeeDateOfJoining = DateExtensions.DateFormate(getJoinDate);
-                    // Console.WriteLine("Your Date of Joining - " + employeeRecordDetail.employeeDateOfJoining);
-                    DateTime datestring = Convert.ToDateTime(employeeRecordDetail.employeeDateOfJoining);
+                    DateTime joiningdatestring = Convert.ToDateTime(employeeRecordDetail.employeeDateOfJoining);
+                    DateTime birthdatestring = Convert.ToDateTime(employeeRecordDetail.employeeDOB);
+
+                    int result = DateTime.Compare(birthdatestring, joiningdatestring);
+                    if (result >= 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(" -> Invalid date! Date of Joining cannot be greater than or equal to Date of Birth.");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        continue;
+                    }
                     DateTime now = DateTime.Now;
-                    var datedifference = Convert.ToInt32((now - datestring).TotalDays);
+                    var datedifference = Convert.ToInt32((now - joiningdatestring).TotalDays);
                     var yeardifference = Convert.ToDecimal(datedifference / 365.2425);
                     employeeRecordDetail.employeeTotalExperience = (double)System.Math.Round(yeardifference, 1);
 
                     joinDate = false;
                 }
-                catch (FormatException)
+                catch (Exception ex)
                 {
-                    Console.WriteLine("Invalid Date Format.");
+                    Console.WriteLine(ex);
                     continue;
                 }
             }
 
-
-            do
+            bool isRemarkValid = false;
+            while (!isRemarkValid)
             {
                 Console.Write(" Enter Employee Remark : ");
                 employeeRecordDetail.employeeRemark = Convert.ToString(Console.ReadLine());
-            } while (!StringValidation(employeeRecordDetail.employeeRemark));
-
-
-            Console.WriteLine("   Enter Employee Department   ");
-            Console.WriteLine(" \n Enter 1 for Development \n Enter 2 for Sales  \n Enter 3 for QA \n Enter 4 for Marketing \n Enter 5 for HR \n Enter 6 for SEO ");
-            Console.Write(" Enter the Number Above Message : ");
-            var userinput = Convert.ToInt16(Console.ReadLine());
-            switch (userinput)
-            {
-                case 1:
-                    employeeRecordDetail.employeeDepartMent = Convert.ToString(EmployeeDepartMentenum.Development);
-                    break;
-                case 2:
-                    employeeRecordDetail.employeeDepartMent = Convert.ToString(EmployeeDepartMentenum.Sales);
-                    break;
-                case 3:
-                    employeeRecordDetail.employeeDepartMent = Convert.ToString(EmployeeDepartMentenum.QA);
-                    break;
-                case 4:
-                    employeeRecordDetail.employeeDepartMent = Convert.ToString(EmployeeDepartMentenum.Marketing);
-                    break;
-                case 5:
-                    employeeRecordDetail.employeeDepartMent = Convert.ToString(EmployeeDepartMentenum.HR);
-                    break;
-                case 6:
-                    employeeRecordDetail.employeeDepartMent = Convert.ToString(EmployeeDepartMentenum.SEO);
-                    break;
-                default:
-                    Console.WriteLine(" Enter Number As Per Above Message ");
-                    break;
+                if (!StringValidation(employeeRecordDetail.employeeRemark))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" -> Invalid Remark. Please enter a valid Remark.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    isRemarkValid = true;
+                }
             }
+
+            bool validInput = false;
+            while (!validInput)
+            {
+                Console.WriteLine("   Enter Employee Department   ");
+                Console.WriteLine(" \n Enter 1 for Development \n Enter 2 for Sales  \n Enter 3 for QA \n Enter 4 for Marketing \n Enter 5 for HR \n Enter 6 for SEO ");
+                Console.Write(" Enter the Number Above Message : ");
+                var userinput = Convert.ToInt16(Console.ReadLine());
+                switch (userinput)
+                {
+                    case 1:
+                        employeeRecordDetail.employeeDepartMent = Convert.ToString(EmployeeDepartMentenum.Development);
+                        validInput = true;
+                        break;
+                    case 2:
+                        employeeRecordDetail.employeeDepartMent = Convert.ToString(EmployeeDepartMentenum.Sales);
+                        validInput = true;
+                        break;
+                    case 3:
+                        employeeRecordDetail.employeeDepartMent = Convert.ToString(EmployeeDepartMentenum.QA);
+                        validInput = true;
+                        break;
+                    case 4:
+                        employeeRecordDetail.employeeDepartMent = Convert.ToString(EmployeeDepartMentenum.Marketing);
+                        validInput = true;
+                        break;
+                    case 5:
+                        employeeRecordDetail.employeeDepartMent = Convert.ToString(EmployeeDepartMentenum.HR);
+                        validInput = true;
+                        break;
+                    case 6:
+                        employeeRecordDetail.employeeDepartMent = Convert.ToString(EmployeeDepartMentenum.SEO);
+                        validInput = true;
+                        break;
+                    default:
+                        Console.WriteLine(" -> Enter Number As Per Above Message ");
+                        break;
+                }
+            }
+
+
 
             try
             {
                 Console.Write(" Enter Employee Monthly Salary : ");
-                employeeRecordDetail.employeeMonthlySalary = Convert.ToInt32(Console.ReadLine());
+                employeeRecordDetail.employeeMonthlySalary = Convert.ToDouble(Console.ReadLine());
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
 
-
-
             employeeDetailList.Add(employeeRecordDetail);
-            employeeDetailList.Sort((x, y) => x.employeeMonthlySalary.CompareTo(y.employeeMonthlySalary));
+            employeeDetailList = employeeDetailList.OrderByDescending(e => e.employeeMonthlySalary).ToList();
             string filePath = ConfigurationManager.AppSettings["file"];
             string JsonConvertFile = JsonConvert.SerializeObject(employeeDetailList);
             using (StreamWriter writer = new StreamWriter(filePath))
--- C#/vsc exam/script.js
+++ C#/vsc exam/script.js
@@ -6,34 +6,34 @@ $(document).ready(function () {
             data.forEach(function (item, index) {
                 item.index = index + 1;
             });
-            var eventTable = $('#eventTable').DataTable({
+            var eventTable = $('#maintable').DataTable({
                 "dom": '<"toolbar">frtip',
                 "searching": true,
                 language: {
                     search: "_INPUT_",
-                    searchPlaceholder: 'Search...'
+                    searchPlaceholder: 'Search here ...'
                 },
                 data: data,
                 columns: [
                     { data: "index", className: 'text-center', orderable: false },
                     { data: "employeeName", className: 'text-center', orderable: true },
                     {
-                        data: null,className: 'text-center', orderable: true,
+                        data: null, className: 'text-center', orderable: true,
                         render: function (data, type, row, meta) {
                             if (row.employeeDepartMent == "Development") { // here is your condition
                                 return '<div class="black-color">' + row.employeeDepartMent + '</div>';
-                            }else if (row.employeeDepartMent == "Sales") {
+                            } else if (row.employeeDepartMent == "Sales") {
                                 return '<div class="red-color">' + row.employeeDepartMent + '</div>';
-                            }else if (row.employeeDepartMent == "Marketing") {
+                            } else if (row.employeeDepartMent == "Marketing") {
                                 return '<div class="green-color">' + row.employeeDepartMent + '</div>';
-                            }else if (row.employeeDepartMent == "QA") {
+                            } else if (row.employeeDepartMent == "QA") {
                                 return '<div class="blue-color">' + row.employeeDepartMent + '</div>';
-                            }else if (row.employeeDepartMent == "HR") {
+                            } else if (row.employeeDepartMent == "HR") {
                                 return '<div class="orange-color">' + row.employeeDepartMent + '</div>';
-                            }else if (row.employeeDepartMent == "SEO") {
+                            } else if (row.employeeDepartMent == "SEO") {
                                 return '<div class="pink-color">' + row.employeeDepartMent + '</div>';
                             }
-                             else {
+                            else {
                                 return row;
                             }
                         },
@@ -44,22 +44,30 @@ $(document).ready(function () {
                         "data": "weblink",
                         render: function (data, type, row) {
                             return (
-                                '<a href="' + data + '">' + row.employeeEmail + '</a>'
+                                '<a href="mailto:">' + row.employeeEmail + '</a>'
                             );
                         },
                     },
                     {
-                        // data: null,
+                        data: null,
                         className: 'text-center',
                         "data": "weblink",
                         render: function (data, type, row) {
                             return (
-                                '<a href="' + data + '">' + row.employeePhoneNo + '</a>'
+                                '<a href="tel:">' + row.employeePhoneNo + '</a>'
                             );
                         },
                         orderable: false
                     },
-                    { data: "employeeGender", className: 'text-center', orderable: false },
+                    {
+                        data: null, className: 'text-center', orderable: false, render: function (data, type, row) {
+                            if (row.employeeGender == "Male") {
+                                return 'M'
+                            } else {
+                                return 'F'
+                            }
+                        }
+                    },
                     {
                         data: null,
                         className: 'text-center',
@@ -73,40 +81,28 @@ $(document).ready(function () {
                 ]
             });
 
-
             $(document).on('click', '.viewdetails', function () {
-                var viewDetails = $(this).attr('view-id');
+                // var viewDetails = $(this).attr('view-id');
                 var row = eventTable.row($(this).closest('tr')).data();
-                $("#name").val(row.employeeName);
-                $("#email").val(row.employeeEmail);
-                $("#dateofBirth").val(row.employeeDOB);
-                $("#gender").val(row.employeeGender);
-                $("#designation").val(row.employeeDesignation);
-                $("#State").val(row.employeeState);
-                $("#city").val(row.employeeCity);
-                $("#postcode").val(row.employeePostCode);
-                $("#phoneno").val(row.employeePhoneNo);
-                $("#department").val(row.employeeDepartMent);
-                $("#monthlysalary").val(row.employeeMonthlySalary);
-                $("#doj").val(row.employeeDateOfJoining);
-                $("#totalexp").val(row.employeeTotalExperience);
-                $("#remark").val(row.employeeRemark);
-
-                console.log(row.employeeName);
-                // $('#exampleModal').modal('show');
-                // console.log(row.employeeEmail);
+                $("#name").text(row.employeeName);
+                $("#email").text(row.employeeEmail);
+                $("#dateofBirth").text(row.employeeDOB);
+                $("#gender").text(row.employeeGender);
+                $("#designation").text(row.employeeDesignation);
+                $("#State").text(row.employeeState);
+                $("#city").text(row.employeeCity);
+                $("#postcode").text(row.employeePostCode);
+                $("#phoneno").text(row.employeePhoneNo);
+                $("#department").text(row.employeeDepartMent);
+                $("#monthlysalary").text("$" + row.employeeMonthlySalary);
+                $("#doj").text(row.employeeDateOfJoining);
+                $("#totalexp").text(row.employeeTotalExperience + ' years ');
+                $("#remark").text(row.employeeRemark);
             });
 
-            var color = 'blue';
-            if ($("#department").val() == "Development") {
-                console.log(data.employeeDepartMent);
-                $("input").css("background", color);
-            }
-
-
         },
-        error: function () {
-            alert('Error retrieving data!');
+        error: function (response) {
+            console.log(response);
         }
     });
 
